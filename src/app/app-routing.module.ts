efc3ebe34dbde26cf13801f8f4326729a05b101b
Ngo Van Tuan <contact.tuannv@gmail.com>
2019-09-25 23:38:33 +0700

    update readme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { CoinComponent } from './coin/coin.component';
+import { MarketComponent } from './market/market.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'coin', component: CoinComponent },
+  { path: 'market', component: MarketComponent },
+  { path: '**', component: MainComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Location, DOCUMENT, isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'TuanASM';
+
+export class AppComponent implements OnInit {
+  constructor(
+    private location: Location
+  ) { }
+
+  invalidSession: boolean;
+  
+  ngOnInit() {
+    if (this.location.path() == "") {
+      this.invalidSession = false;
+    } else {
+      this.invalidSession = true;
+    }
+  }
 }
